007f740876814c7d8c1a55e1b5f5fbfcfdc18df5
fainstar <37238989+fainstar@users.noreply.github.com>
2023-11-11 14:10:33 +0800

    TEST

--- arduino/main.ino
+++ arduino/main.ino
@@ -1,18 +1,37 @@
-#include "functions.h"
+#include <SimpleDHT.h>
 
-/// @brief
-void setup()
-{
+int pinDHT11 = A0;
+SimpleDHT11 dht11;
 
-    Serial.begin(9600);
+void setup() {
+  Serial.begin(9600);
+  pinMode(A1, INPUT);  
+  pinMode(2, INPUT);  
 
-    setupFunctions();
 }
 
-/// @brief
-void loop()
-{
-    performTask();
+void loop() {
+  int num = analogRead(A1);  
+  int num2 = digitalRead(2);  
 
-    // 其他主程式的程式碼...
+  byte temperature = 0;
+  byte humidity = 0;
+    int err = SimpleDHTErrSuccess;
+    Serial.println("=================================");
+    if ((err = dht11.read(pinDHT11, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+       Serial.print("Read DHT11 failed, err="); Serial.println(err);delay(1000);
+       return;
+    }
+  
+  Serial.print("土壤 = ");
+  Serial.println(num);
+   Serial.print("下雨 = ");
+  Serial.println(num2);
+  Serial.print("湿度 = ");
+  Serial.println((int)humidity);
+  Serial.print("温度 = ");
+  Serial.print((int)temperature);
+  Serial.println("C");
+
+  delay(500);  // 每3秒显示一次
 }
